5c985ef8f5a841225492dc78a866a08d8aff6d65
Maty <matienzof6@gmail.com>
2024-11-06 12:34:15 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+
+
+
+device = cv2.VideoCapture(0)
+if not device.isOpened():
+    print("No se puede abrir la camara")
+    exit()
+while True:
+    cam, frame = device.read()
+    if not cam:
+        print("No se pudo leer de la cámara")
+        break
+    cv2.imshow("Image", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+device.release()
+cv2.destroyAllWindows()
