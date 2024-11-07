3dc926dd4029d3ac360cdc59181c474b3473f5ce
Maty <matienzof6@gmail.com>
2024-11-07 11:19:02 +0100

    Delete innc¡ecesary mouth detection and create logic to pause/play videos

--- main.py
+++ main.py
@@ -35,6 +35,10 @@ print("[INFO] starting video stream...")
 device = VideoStream(src=0).start()
 time.sleep(2.0)
 
+# variable to control the stop statement
+
+is_paused = False
+
 # loop over the frames from the video stream
 while True:
     # grab the frame from the video stream, resize it, and convert it to grayscale
@@ -46,34 +50,34 @@ while True:
     faceRects = detectors["face"].detectMultiScale(
 		gray, scaleFactor=1.05, minNeighbors=5, minSize=(30, 30),
 		flags=cv2.CASCADE_SCALE_IMAGE)
+    if len(faceRects) == 0:
+        # Pause the video only if the video is not paused yet
+        if not is_paused:
+            print("No se detectaron caras - Pausando el video")
+            # Aquí va el código para pausar el video (ejemplo: una función que presiona la tecla de pausa)
+            # pause_video()
+            is_paused = True  # change the state to "paused" to avoid loops
+    else:
+        # If the faces are detected the video will continue
     
     # loop over the face bounding boxes
-    for (fX, fY, fW, fH) in faceRects:
-		# extract the face ROI
-        faceROI = gray[fY:fY+ fH, fX:fX + fW]
-		# apply eyes detection to the face ROI
-        eyeRects = detectors["eyes"].detectMultiScale(
-                faceROI, scaleFactor=1.1, minNeighbors=10,
-                minSize=(15, 15), flags=cv2.CASCADE_SCALE_IMAGE)
-            # apply smile detection to the face ROI
-        smileRects = detectors["smile"].detectMultiScale(
-                faceROI, scaleFactor=1.1, minNeighbors=10,
-                minSize=(15, 15), flags=cv2.CASCADE_SCALE_IMAGE)
-        # loop over the eye bounding boxes
-        for (eX, eY, eW, eH) in eyeRects:
-			# draw the eye bounding box
-            ptA = (fX + eX, fY + eY)
-            ptB = (fX + eX + eW, fY + eY + eH)
-            cv2.rectangle(frame, ptA, ptB, (0, 0, 255), 2)
-		# loop over the smile bounding boxes
-        for (sX, sY, sW, sH) in smileRects:
-			# draw the smile bounding box
-            ptA = (fX + sX, fY + sY)
-            ptB = (fX + sX + sW, fY + sY + sH)
-            cv2.rectangle(frame, ptA, ptB, (255, 0, 0), 2)
-		# draw the face bounding box on the frame
-        cv2.rectangle(frame, (fX, fY), (fX + fW, fY + fH),
-			(0, 255, 0), 2)
+        for (fX, fY, fW, fH) in faceRects:
+            # extract the face ROI
+            faceROI = gray[fY:fY+ fH, fX:fX + fW]
+            # apply eyes detection to the face ROI
+            eyeRects = detectors["eyes"].detectMultiScale(
+                    faceROI, scaleFactor=1.1, minNeighbors=10,
+                    minSize=(15, 15), flags=cv2.CASCADE_SCALE_IMAGE)
+            # loop over the eye bounding boxes
+            for (eX, eY, eW, eH) in eyeRects:
+                # draw the eye bounding box
+                ptA = (fX + eX, fY + eY)
+                ptB = (fX + eX + eW, fY + eY + eH)
+                cv2.rectangle(frame, ptA, ptB, (0, 0, 255), 2)
+            # draw the face bounding box on the frame
+            cv2.rectangle(frame, (fX, fY), (fX + fW, fY + fH),
+                (0, 255, 0), 2)
+
 
 
 
@@ -85,5 +89,8 @@ while True:
     if key == ord("q"):
         break
     # do a bit of cleanup
+
+
+
 cv2.destroyAllWindows()
 device.stop()
